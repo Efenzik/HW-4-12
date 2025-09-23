46a5559ac5db3d9f63d5800d4b23732acdbb1cf8
Artem Cherkashin <157517719+Efenzik@users.noreply.github.com>
2025-09-23 16:49:09 +0300

    Ended

--- script.js
+++ script.js
@@ -1,63 +1,73 @@
 class CountdownTimer {
   constructor({ selector, targetTimeEnd }) {
-    const timerEl = document.querySelector(selector);
-    const targetTime = targetTimeEnd;
+    this.timerEl = document.querySelector(selector);
+    this.targetTime = targetTimeEnd;
+    this.intervalId = null;
 
-    const daysCount = timerEl.querySelector('[data-value="days"]');
-    const hoursCount = timerEl.querySelector('[data-value="hours"]');
-    const minsCount = timerEl.querySelector('[data-value="mins"]');
-    const secsCount = timerEl.querySelector('[data-value="secs"]');
-    const TIME_COUNT = 100;
-    function updateTimer() {
-      const now = new Date();
-      const time = targetTime - now;
+    this.daysCount = this.timerEl.querySelector('[data-value="days"]');
+    this.hoursCount = this.timerEl.querySelector('[data-value="hours"]');
+    this.minsCount = this.timerEl.querySelector('[data-value="mins"]');
+    this.secsCount = this.timerEl.querySelector('[data-value="secs"]');
+  }
 
-      if (time <= 0) {
-        clearInterval(intervalId);
-        daysCount.textContent = "00";
-        hoursCount.textContent = "00";
-        minsCount.textContent = "00";
-        secsCount.textContent = "00";
-        return;
-      }
+  start() {
+    this.updateTimer();
+    this.intervalId = setInterval(() => this.updateTimer(), 1000);
+  }
 
-      /*
-       * Дні, що залишилися: ділимо значення UTC на 1000 * 60 * 60 * 24, кількість
-       * мілісекунд в один день (мілісекунди * секунди * хвилини * години)
-       */
-      const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
 
-      /*
-       * Решта годин: отримуємо залишок від попереднього розрахунку за допомогою оператора
-       * залишку% і ділимо його на кількість мілісекунд в одній годині
-       * (1000 * 60 * 60 = мілісекунди * хвилини * секунди)
-       */
-      const hours = Math.floor(
-        (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
+  updateTimer() {
+    const now = new Date();
+    const time = this.targetTime - now;
 
-      /*
-       * Решта хвилин: отримуємо хвилини, що залишилися і ділимо їх на кількість
-       * мілісекунд в одній хвилині (1000 * 60 = мілісекунди * секунди)
-       */
-      const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-      /*
-       * Решта секунд: отримуємо секунди, які залишилися і ділимо їх на кількість
-       * миллисекунд в одной секунде (1000)
-       */
-      const secs = Math.floor((time % (1000 * 60)) / 1000);
-      daysCount.textContent = days.toString().padStart(2, "0");
-      hoursCount.textContent = hours.toString().padStart(2, "0");
-      minsCount.textContent = mins.toString().padStart(2, "0");
-      secsCount.textContent = secs.toString().padStart(2, "0");
+    if (time <= 0) {
+      this.stop();
+      this.daysCount.textContent = "00";
+      this.hoursCount.textContent = "00";
+      this.minsCount.textContent = "00";
+      this.secsCount.textContent = "00";
+      return;
     }
-    updateTimer();
-    const intervalId = setInterval(updateTimer, TIME_COUNT);
+
+    /*
+     * Дні, що залишилися: ділимо значення UTC на 1000 * 60 * 60 * 24, кількість
+     * мілісекунд в один день (мілісекунди * секунди * хвилини * години)
+     */
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+    /*
+     * Решта годин: отримуємо залишок від попереднього розрахунку за допомогою оператора
+     * залишку% і ділимо його на кількість мілісекунд в одній годині
+     * (1000 * 60 * 60 = мілісекунди * хвилини * секунди)
+     */
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    /*
+     * Решта хвилин: отримуємо хвилини, що залишилися і ділимо їх на кількість
+     * мілісекунд в одній хвилині (1000 * 60 = мілісекунди * секунди)
+     */
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+
+    /*
+     * Решта секунд: отримуємо секунди, які залишилися і ділимо їх на кількість
+     * миллисекунд в одной секунде (1000)
+     */
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+    this.daysCount.textContent = days.toString().padStart(2, "0");
+    this.hoursCount.textContent = hours.toString().padStart(2, "0");
+    this.minsCount.textContent = mins.toString().padStart(2, "0");
+    this.secsCount.textContent = secs.toString().padStart(2, "0");
   }
 }
 
-new CountdownTimer({
+const timer = new CountdownTimer({
   selector: "#timer-1",
   targetTimeEnd: new Date("Sep 25 2025 17:13:52"),
 });
+
+timer.start();
